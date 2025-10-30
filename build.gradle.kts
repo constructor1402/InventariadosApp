db85c1d92d530c0e7d56010cb6a577dd9892b68c
Andres Bayon <andresbayon22@gmail.com>
2025-10-30 18:49:34 -0500

    intento andres bayon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // build.gradle.kts (nivel de proyecto)
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
